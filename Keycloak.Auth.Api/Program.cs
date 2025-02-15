743f20c0ea778be956b1c6da39051a1ca27e00de
yaroslavx <yakupov.yaroslav@icloud.com>
2025-02-15 21:29:42 +0300

    MAINTAIN: For swagger added keycloak auth

--- Keycloak.Auth.Api/Program.cs
+++ Keycloak.Auth.Api/Program.cs
@@ -1,7 +1,12 @@
+using System.Security.Claims;
+using Keycloak.Auth.Api.Extensions;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGenWithAuth(builder.Configuration);
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication();
 
 WebApplication app = builder.Build();
 
@@ -10,6 +15,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("users/me", (ClaimsPrincipal claimsPrincipal) =>
+{
+    return claimsPrincipal.Claims.ToDictionary(c => c.Type, c => c.Value);
+}).RequireAuthorization();
 
 app.UseAuthentication();
 
